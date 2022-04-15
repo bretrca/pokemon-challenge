44ec0fcd39c84e0b03513408e937146a79ffacea
Brayan Trujillo <briann320@gmail.com>
2022-04-15 13:33:46 +0200

    [FIX] react 18 corrections

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import Index from "../src/js/index.js"
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Index tab= "home"/>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
